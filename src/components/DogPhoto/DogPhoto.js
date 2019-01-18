3e0ed3feba42c16421ffabe00ca997225f191726
ashu8912 <ashughildiyal5@gmail.com>
2019-01-18 10:35:26 +0530

    polling and refetching

--- src/components/DogPhoto/DogPhoto.js
+++ src/components/DogPhoto/DogPhoto.js
@@ -10,15 +10,36 @@ const GET_DOG_PHOTO = gql`
     }
   }
 `;
+//refetch is another argument to render prop function
+//which helps us reload the query on an event rather 
+//at every certain interval
+//we just have to call the refetch function on an event
+/*
+{({ loading, error, data, refetch }) => {
+      if (loading) return null;
+      if (error) return `Error!: ${error}`;
+
+      return (
+        <div>
+          <img
+            src={data.dog.displayImage}
+            style={{ height: 100, width: 100 }}
+          />
+          <button onClick={() => refetch()}>Refetch!</button>
+        </div>
+      );
+    }}
+*/
 const DogPhoto=({breed})=>(
     breed?
-    <Query query={GET_DOG_PHOTO} variables={{breed}}>
-    {({loading,error,data})=>{
+    <Query query={GET_DOG_PHOTO} variables={{breed}}
+    >
+    {({loading,error,data,startPolling,stopPolling})=>{
         if(loading) return <p>loading...</p>
-        if(error) return <p>error</p>;
+        if(error) return <p>error...</p>;
         console.log(data);
-        return <img src={data.dog.displayImage} 
-        alt="dog"/>
+        return <div><img src={data.dog.displayImage} 
+        alt="dog"/></div>
     }}
     </Query>:<p>Select a breed</p>
 )
